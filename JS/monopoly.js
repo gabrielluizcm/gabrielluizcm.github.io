a082b0137129882f0311c1e5cb5bd8bec4f00dc9
Gabriel Luiz <gabrielluizcm@hotmail.com>
2019-11-18 17:15:29 -0300

    Teste bash e onorientation.change

--- JS/monopoly.js
+++ JS/monopoly.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function ()
 {
     window.onbeforeunload = confirmarSaida
     window.onload = responsiveness
-    //window.onresize = responsiveness
+    window.onorientationchange = responsiveness
     screen.orientation.lock('portrait-primary')
 
     if (localStorage.length == 0)
